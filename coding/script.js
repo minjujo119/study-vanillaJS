e1ddc45fb88b77df5cb22ff47a6f359821cd3845
minju <108469517+minjujo119@users.noreply.github.com>
2023-02-06 16:08:51 +0900

    js 파일 생성

---
+++ coding/script.js
@@ -0,0 +1,29 @@
+
+const key = {
+  keyDown : {},
+  keyValue : {
+    37:'left',
+    38:'up',
+    39:'right',
+    40:'down',
+  }
+};
+
+const windowEvent = ()=>{
+window.addEventListener('keydown',e =>{
+  key.keyDown[key.keyValue[e.which]] = true;
+  console.log(key.keyDown);
+});
+window.addEventListener('keyup',e =>{
+  key.keyDown[key.keyValue[e.which]] = false;
+  console.log(key.keyDown);
+})
+};
+
+const init = ()=>{
+  windowEvent();
+}
+
+window.onload = ()=>{
+  init();
+}
