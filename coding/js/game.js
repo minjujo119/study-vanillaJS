f0ed5f8b91bdb8bd07026f4449727bdc6b6f2fc8
minju <minjujo119@gmail.com>
2023-02-06 16:48:28 +0900

    소스파일 업로드

---
+++ coding/js/game.js
@@ -0,0 +1,30 @@
+const key = {
+  keyDown : {},
+  keyValue : {
+    37:'left',
+    38:'up',
+    39:'right',
+    40:'down'
+  }
+};
+const windowEvent = () => {
+
+  window.addEventListener('keydown', e => {
+    key.keyDown[key.keyValue[e.key]] = true;
+    console.log(key.keyDown); 
+  });
+
+  window.addEventListener('keyup', e => {
+    key.keyDown[key.keyValue[e.key]] = false;
+    console.log(key.keyDown);
+  });
+
+};
+
+const init = () => {
+  windowEvent();
+};
+
+window.onload = () => {
+  init();
+};
