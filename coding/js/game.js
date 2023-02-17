eb6d611aac326fe662fadd595462a206dc4145eb
minju <minjujo119@gmail.com>
2023-02-17 16:41:32 +0900

    캐릭터 이동 개발중

--- coding/js/game.js
+++ coding/js/game.js
@@ -1,31 +1,50 @@
+
 const key = {
   keyDown : {},
   keyValue : {
     37:'left',
-    38:'up',
     39:'right',
-    40:'down'
+    88:'attack',
   }
+}
+
+const renderGame = () => {
+  window.requestAnimationFrame(renderGame);
 }
 
 const windowEvent = () => {
   window.addEventListener('keydown', e => {
     if(key.keyValue[e.which] !== undefined){
       key.keyDown[key.keyValue[e.which]] = true;
-      console.log(key.keyDown); 
+      hero.keyMotion();
     }
   });
 
   window.addEventListener('keyup', e => {
     if(key.keyValue[e.which] !== undefined){
       key.keyDown[key.keyValue[e.which]] = false;
-      console.log(key.keyDown);  
+      hero.keyMotion();
     }
   });
 }
 
+
+const loadImg = () => {
+  const preLoadImgSrc = ['../lib/images/ninja_idle.png','../lib/images/ninja_run.png','../lib/images/ninja_attack.png']
+  preLoadImgSrc.forEach(arr => {
+    const img = new Image();
+    img.src = arr;
+  })
+}
+
+
+
+let hero;
 const init = () => {
+  hero = new Hero('.hero');
+  loadImg();
   windowEvent();
+  renderGame();
 }
 window.onload = () => {
   init();
